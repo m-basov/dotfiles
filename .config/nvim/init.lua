4ed026fdd45af17e917d116c07651b02a3be5084
Mykola Basov <git@mbasov.com>
2025-08-17 18:48:55 +0100

    move requires to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,7 @@
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+require("config.options")
+require("config.keymaps")
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
